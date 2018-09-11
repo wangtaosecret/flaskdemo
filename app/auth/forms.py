8bf743c758c1b9807da9e41cb7165864ac22874d
wangtao <wang.tao@dewmobile.net>
2018-09-11 15:35:33 +0800

    FlaskForm

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,10 +1,10 @@
 # coding:utf8
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import Required, Length, Email, Regexp, EqualTo
 from wtforms import ValidationError
 from ..models import User
-class LoginForm(Form):
+class LoginForm(FlaskForm):
 	email = StringField('Email', validators=[Required(), Length(1, 64),
 		Email()])
 	password = PasswordField('Password', validators=[Required()])
@@ -14,7 +14,7 @@ class LoginForm(Form):
 
 
 
-class RegistrationForm(Form):
+class RegistrationForm(FlaskForm):
 	email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
 	username = StringField('Username', validators=[Required(), Length(1, 64), 
 		Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 'Usernames must have only letters, numbers,dots or underscores')])
@@ -32,7 +32,7 @@ class RegistrationForm(Form):
 			raise ValidationError('Username already in use.')
 
 # 修改密码
-class ChangePasswordForm(Form):
+class ChangePasswordForm(FlaskForm):
 	old_password = PasswordField('Old password', validators=[Required()])
 	password = PasswordField('New password', validators=[Required(), EqualTo('password2',
 		message='Passwords must match.')])
@@ -40,18 +40,18 @@ class ChangePasswordForm(Form):
 	submit = SubmitField('Update Password')
 
 # 发出重置密码请求道邮箱
-class PasswordResetRequestForm(Form):
+class PasswordResetRequestForm(FlaskForm):
 	email = StringField('Email', validators=[Required(), Length(1, 64), Email()])
 	submit = SubmitField('Reset Password')
 # 重置密码
-class PasswordResetForm(Form):
+class PasswordResetForm(FlaskForm):
 	password = PasswordField('New Password', validators=[Required(),
 		EqualTo('password2', message='Passwords must match.')])
 	password2 = PasswordField('Confirm password', validators=[Required()])
 	submit = SubmitField('Reset Password')
 
 
-class ChangeEmailForm(Form):
+class ChangeEmailForm(FlaskForm):
 	email = StringField('New Email', validators=[Required(), Length(1, 64), Email()])
 	password = PasswordField('Password', validators=[Required()])
 	submit = SubmitField('Update Email Address')
